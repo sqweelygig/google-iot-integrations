b1f170fc4025972a8e280a1891bf0780e379903c
Andrew Lucas <sqweelygig@googlemail.com>
2021-04-11 17:55:56 +0100

    Adjust default time to sixteen hours

--- remember_all/remember_all.py
+++ remember_all/remember_all.py
@@ -11,7 +11,7 @@ class RememberAll:
 		self,
 		calendars,
 		slot_count=24,
-		seconds_considered=12*60*60,
+		seconds_considered=16*60*60,
 		credentials_path='service-account.json',
 		smallest_slot=60,
 		setup_time=0.5,
