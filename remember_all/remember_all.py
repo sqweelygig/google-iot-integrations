108a6837e18e74930b6373ed731699bf2b2fcc7e
Andrew Lucas <sqweelygig@googlemail.com>
2021-04-11 17:54:31 +0100

    Adjust default time to one day

--- remember_all/remember_all.py
+++ remember_all/remember_all.py
@@ -11,7 +11,7 @@ class RememberAll:
 		self,
 		calendars,
 		slot_count=24,
-		seconds_considered=24*60*60*2,
+		seconds_considered=24*60*60,
 		credentials_path='service-account.json',
 		smallest_slot=60,
 		setup_time=0.5,
